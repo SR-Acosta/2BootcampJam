de1b5b6050be039668c2a64fd97bdcc9fa9865ac
Santiago Acosta <acosta.robayo27@gmail.com>
2023-05-24 12:27:47 -0500

    Spawn Sytem & mejora de Enemy IA

--- 2BootcampJam/Assets/AcostaWork/Scripts/enemySawPlayer.cs
+++ 2BootcampJam/Assets/AcostaWork/Scripts/enemySawPlayer.cs
@@ -9,12 +9,25 @@ public class enemySawPlayer : MonoBehaviour
    public AIDestinationSetter targetManager;
    public Transform playerDetected;
 
-   
+   private void Start()
+   {
+      targetManager.target = GameObject.FindWithTag("Mula").transform;
+      playerDetected = GameObject.FindWithTag("Player").transform;
+   }
+
    private void OnTriggerStay2D(Collider2D other)
    {
       if (other.gameObject.tag == "Player")
       {
          targetManager.target = playerDetected;
+      }
+   }
+
+   private void OnTriggerExit2D(Collider2D other)
+   {
+      if (other.gameObject.tag == "Player")
+      {
+         targetManager.target = GameObject.FindWithTag("Mula").transform;
       }
    }
 }
