01a7779632c1604180c9010e4d79ee9c602173fb
Santiago Acosta <acosta.robayo27@gmail.com>
2023-05-24 20:09:03 -0500

    Enemy life bar

--- 2BootcampJam/Assets/AcostaWork/Scripts/enemyLife.cs
+++ 2BootcampJam/Assets/AcostaWork/Scripts/enemyLife.cs
@@ -1,9 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class enemyLife : MonoBehaviour
 {
-    
+    public Slider slider;
+    public float lifeCount;
+    public Gradient gradient;
+    public Image fill;
+    private void Start()
+    {
+        slider.maxValue = lifeCount;
+        slider.value = slider.maxValue;
+        slider = GetComponentInChildren<Slider>();
+        slider.gameObject.SetActive(false);
+        fill.color = gradient.Evaluate(1f);
+    }
+    private void Update()
+    {
+        if (slider.value < slider.maxValue)
+        {
+            slider.gameObject.SetActive(true);
+        }
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
 }
