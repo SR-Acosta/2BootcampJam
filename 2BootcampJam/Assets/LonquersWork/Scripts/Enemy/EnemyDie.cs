e3b4bc8d6cf3a209b30d199f212010753cd41c17
FabioBetancourt <fabiobetancourt7@gmail.com>
2023-05-26 13:10:51 -0500

    Script enemyDie terminado

--- 2BootcampJam/Assets/LonquersWork/Scripts/Enemy/EnemyDie.cs
+++ 2BootcampJam/Assets/LonquersWork/Scripts/Enemy/EnemyDie.cs
@@ -6,41 +6,90 @@ namespace LonquersWork.Scripts
 {
     public class EnemyDie : MonoBehaviour
     {
+        [Serializable]
+        public class ItemDrop
+        {
+            public GameObject itemPrefab;
+            public int dropProbability;
+        }
+
         [Header("Item configuration")]
-        [Tooltip("You need to drag the prefab of the item that we want the enemy to drop, note that it must be a prefab")]
-        // Prefab of the item that we want the enemy to drop
-        public GameObject itemPrefab;
-        
-        [Header("Min Value configuration")]
-        [Tooltip("this is the minimum value that the random number can take")]
-        public int minValue = 0;
-        [Header("Max Value configuration")]
-        [Tooltip("This is the maximum value that the random number can take")]
-        public int maxValue = 0;
-        [Header("Lottery configuration")]
-        [Tooltip("This is the number that the random number must match to drop the item")]
-        public int lottery = 0;
-        
+        [Tooltip("You need to drag the prefabs of the items that the enemy can drop, note that they must be prefabs")]
+        public ItemDrop[] itemDrops;
+
+        [Tooltip("Probability of no item drop (in percentage)")]
+        [Range(0, 100)]
+        public int noDropProbability = 10;
+
+        [Header("Drop Range configuration")]
+        [Tooltip("This is the maximum distance from the enemy where the item can drop")]
+        public float dropRange = 2f;
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             // If the player passes over the enemy
             if (other.gameObject.CompareTag("Player"))
             {
-                // we defeat the enemy
+                // We defeat the enemy
                 Defeat();
             }
         }
-   
+
         // We call this function when the enemy is defeated
         private void Defeat()
         {
-            float randomNumber = Random.Range(minValue, maxValue);
-            Debug.Log(randomNumber);
-            if (Mathf.Approximately(randomNumber, lottery))
+            int totalProbability = CalculateTotalProbability();
+
+            int randomValue = Random.Range(0, totalProbability);
+
+            if (randomValue < noDropProbability)
             {
-                Instantiate(itemPrefab, transform.position, Quaternion.identity);
+                // No item dropped
+                Destroy(gameObject);
+                return;
             }
+
+            int cumulativeProbability = noDropProbability;
+            foreach (var itemDrop in itemDrops)
+            {
+                cumulativeProbability += itemDrop.dropProbability;
+                if (randomValue < cumulativeProbability)
+                {
+                    InstantiateItem(itemDrop.itemPrefab);
+                    break;
+                }
+            }
+
             Destroy(gameObject);
+        }
+
+        // Calculate the total probability of item drops
+        private int CalculateTotalProbability()
+        {
+            int totalProbability = noDropProbability;
+            foreach (var itemDrop in itemDrops)
+            {
+                totalProbability += itemDrop.dropProbability;
+            }
+            return totalProbability;
+        }
+
+        // Instantiate the item at a random drop position
+        private void InstantiateItem(GameObject itemPrefab)
+        {
+            Vector2 dropPosition = GetRandomDropPosition();
+            Instantiate(itemPrefab, dropPosition, Quaternion.identity);
+        }
+
+        // Generate a random position near the enemy for item drop
+        private Vector2 GetRandomDropPosition()
+        {
+            Vector2 enemyPosition = transform.position;
+            float offsetX = Random.Range(-dropRange, dropRange);
+            float offsetY = Random.Range(-dropRange, dropRange);
+
+            Vector2 dropPosition = enemyPosition + new Vector2(offsetX, offsetY);
+            return dropPosition;
         }
     }
 }
