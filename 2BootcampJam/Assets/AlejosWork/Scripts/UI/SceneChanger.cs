54a0ad8b5f612e24db6a9336767d067d89152bba
Onibokuro <132405405+Onibokuro@users.noreply.github.com>
2023-05-30 10:42:47 -0500

    UI menus principales
    Menu principal, menu de pausa, creditos

--- 2BootcampJam/Assets/AlejosWork/Scripts/UI/SceneChanger.cs
+++ 2BootcampJam/Assets/AlejosWork/Scripts/UI/SceneChanger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,53 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    public bool canPause;
+    [SerializeField]
+    private GameObject pauseMenu;
+    private static bool isPaused;
+    private void Awake()
+    {
+        SceneAwaker();
+    }
+    private void Update()
+    {
+        if (canPause && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Pause Method");
+            ScenePauser();
+        }
+    }
     public void SceneMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
     public void SceneGame()
     {
+        isPaused = false;
         SceneManager.LoadScene("Scene1");
     }
     public void SceneDefeat()
     {
         SceneManager.LoadScene("DefeatScreen");
+    }
+    public void ScenePauser()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        }         
+    }
+
+    public void SceneAwaker()
+    {
+        isPaused = false;
+        Time.timeScale = 1;
     }
 }
