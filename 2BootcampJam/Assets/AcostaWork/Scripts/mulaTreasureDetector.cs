2333391ab0939c9361f9ef5c810aacd3c1ec2b60
Santiago Acosta <acosta.robayo27@gmail.com>
2023-06-02 10:15:50 -0500

    basura

--- 2BootcampJam/Assets/AcostaWork/Scripts/mulaTreasureDetector.cs
+++ 2BootcampJam/Assets/AcostaWork/Scripts/mulaTreasureDetector.cs
@@ -33,6 +33,8 @@ public class mulaTreasureDetector : MonoBehaviour
             targetManager.target = reloadPlayer;
         }
     }
+    
+    
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.tag == "Treasure")
